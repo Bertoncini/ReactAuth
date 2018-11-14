93582c48ab9e5e355f505f905a04dbcc1b42be43
Bertoncini <alex.bertoncini@terra.com.br>
2018-11-13 23:19:46 -0200

    adicionado botao de login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Auth from './components/auth';
 import App from './App';
 import Protected from './pages/protected';
 import Public from './pages/public';
+import Login from './components/login';
 import * as serviceWorker from './serviceWorker';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Redirect,
   withRouter
 } from 'react-router-dom';
@@ -17,11 +18,32 @@ ReactDOM.render(
 <Router>
     <div>
         <Route path="/" component={App} />
+        <Route path="/login" component={Login} />
         <Route path="/public" component={Public} />
-        <Route path="/protected" component={Protected} />        
+        <PrivateRoute path="/protected" component={Protected} />        
     </div>
 </Router>
     , document.getElementById('root'));
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        Auth.fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
